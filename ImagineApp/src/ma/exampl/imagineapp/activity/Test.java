1cfc7078a950e0ab3d0f59dab93ef5bdeeb03f40
SoufianeTA <tr.soufiane@hotmail.fr>
2015-03-08 13:14:31 +0000

    commit

--- ImagineApp/src/ma/exampl/imagineapp/activity/Test.java
+++ ImagineApp/src/ma/exampl/imagineapp/activity/Test.java
@@ -1,7 +1,14 @@
 package ma.exampl.imagineapp.activity;
 
+import android.R;
 import android.app.Activity;
+import android.os.Bundle;
 
-public class Test extends Activity{
+public class Test extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
 
+	}
 }
